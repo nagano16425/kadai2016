ed4c40ede96db5c5cc1542436c81d6dd9609ad00
nagano16425 <sd16425@toyota-ti.ac.jp.com>
2016-04-19 21:55:34 +0900

    kadai2.4&2.5 2016/04/19

--- cat.py
+++
@@ -1,9 +0,0 @@
-import sys    # 標準ライブラリをインポート
-
-if __name__ == "__main__":    # if文のブロック(スクリプトファイルとして実行された場合)
-
-    param = sys.argv
-
-    f = open (param[1], "r", encoding="utf-8")
-    for line in f:
-        print(line, end=" ")
--- cat_cv000.py
+++
@@ -1,3 +0,0 @@
-f = open ("cv000_29590.txt", "r", encoding="utf-8")
-for line in f:
-    print(line, end=" ")
